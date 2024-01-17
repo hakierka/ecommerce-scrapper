2930fd7bda3f3e845c0f6a2eaf61809641bac2a8
Amy Waliszewska <69154712+hakierka@users.noreply.github.com>
2024-01-17 21:37:23 +0100

    Update index.js with comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Import the scrapeProducts function from the 'api' module
 const { scrapeProducts } = require('./api');
-
+// Call the scrapeProducts function to initiate the scraping process
 scrapeProducts();
